d61737ccf3101cdf082dc87969dd2bd5d46424cd
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-18 17:42:45 +0500

    Task2

---
+++ Tyuiu.KozyrevRA.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using Tyuiu.KozyrevRA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.KozyrevRA.Sprint1.Task2.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSideSquare(x);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint1.Task2.V5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.KozyrevRA.Sprint1.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Формулировка задания: Известна длина стороны куба. Вычислить площадь    *");
+            Console.WriteLine("* боковой поверхности куба.                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Что пользователь вводит? Длина стороны куба (целое число)               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Что программа печатает на экране? Площадь боковой поверхности           *");
+            Console.WriteLine("* куба (целое число)                                                      *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Площадь боковой поверхности куба со стороной X = "+ ds.CalculateSideSquare(x));
+
+            Console.ReadLine();
+        }
+    }
+}
