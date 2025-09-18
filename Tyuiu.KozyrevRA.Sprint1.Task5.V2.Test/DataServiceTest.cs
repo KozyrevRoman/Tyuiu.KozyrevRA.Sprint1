25bf13d15cdba3cd60027bcd5a9462e51c98ab4d
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-18 23:00:58 +0500

    Task5

---
+++ Tyuiu.KozyrevRA.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KozyrevRA.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.KozyrevRA.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 100.0;
+            double res = ds.FahrenheitToСelsius(x);
+            int result = Convert.ToInt32(res);
+            int wait = 38;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.KozyrevRA.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Дано значение температуры в градусах Фаренгейта. Определить             *");
+            Console.WriteLine("* значение этой же температуры в градусах Цельсия.                        *");
+            Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("Введите значение температуры в градусах Фаренгейта:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Значений этой же температуры в градусах Цельсия:                        *");
+            Console.WriteLine(ds.FahrenheitToСelsius(x));
+            Console.WriteLine("***************************************************************************");
+
+            Console.ReadKey();
+        }
+    }
+}
